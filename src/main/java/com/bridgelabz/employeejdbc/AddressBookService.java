a935c385ec5526cf3eb7c51239b1c992c5a1bd2a
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-09-24 10:10:34 +0530

    [Vaibhav] added ability to update contact and sync with db

--- src/main/java/com/bridgelabz/employeejdbc/AddressBookService.java
+++ src/main/java/com/bridgelabz/employeejdbc/AddressBookService.java
@@ -9,33 +9,46 @@ public class AddressBookService
 {
 public AddressBookJdbc addressBookJdbc;
 	
-	public int getQuery() throws SQLException {
-		String query = "select p.firstname,p.lastname,p.phonenumber,p.email,a.residence_address,a.city,a.state,a.country,a.zip,r.type\r\n" + 
-				"from address a,address_book ab,person_details p,relation_type r\r\n" + 
-				"where ab.person_id = p.person_id\r\n" + 
-				"and ab.relation_id = r.relation_id\r\n" + 
-				"and p.person_id = a.person_id;";
-		ResultSet queries = getQuerries(query);
-		return printSet(queries);
-	}
-	
-	private int printSet(ResultSet queries) throws SQLException {
-		int i=0;
-		while(queries.next()) {
-			i++;
-			String firstname = queries.getString("p.firstname");
-			String lastname = queries.getString("p.lastname");
-			System.out.println(firstname+" "+lastname);
-		}
-		return i;
-	}
+public int getQuery() throws SQLException {
+	String query = "select p.firstname,p.lastname,p.phonenumber,p.email,a.residence_address,a.city,a.state,a.country,a.zip,r.type\r\n" + 
+			"from address a,address_book ab,person_details p,relation_type r\r\n" + 
+			"where ab.person_id = p.person_id\r\n" + 
+			"and ab.relation_id = r.relation_id\r\n" + 
+			"and p.person_id = a.person_id;";
+	ResultSet queries = getQuerries(query);
+	return printSet(queries);
+}
 
-	private ResultSet getQuerries(String query) throws SQLException {
-		addressBookJdbc = AddressBookJdbc.getInstance();
-		Connection connection = addressBookJdbc.dbConnect();
-		Statement statement = connection.createStatement();
-		
-		return statement.executeQuery(query);
+private int printSet(ResultSet queries) throws SQLException {
+	int i=0;
+	while(queries.next()) {
+		i++;
+		String firstname = queries.getString("p.firstname");
+		String lastname = queries.getString("p.lastname");
+		System.out.println(firstname+" "+lastname);
 	}
+	return i;
+}
+
+private ResultSet getQuerries(String query) throws SQLException {
+	addressBookJdbc = AddressBookJdbc.getInstance();
+	Connection connection = addressBookJdbc.dbConnect();
+	Statement statement = connection.createStatement();
+	
+	return statement.executeQuery(query);
+}
+
+public boolean getUpdate(String firstName, String lastName, String email) throws SQLException {
+	String query = String.format("update person_details set email = '%s' where firstname = '%s' and lastname = '%s';",email,firstName,lastName);
+	addressBookJdbc = AddressBookJdbc.getInstance();
+	Connection connection = addressBookJdbc.dbConnect();
+	Statement statement = connection.createStatement();
+	int z = statement.executeUpdate(query);
+	ResultSet queries = getQuerries(String.format("Select email from person_details where firstname = '%s' and lastname = '%s';",firstName,lastName));
+	while(queries.next()) {
+		if (queries.getString("email").equals(email)) return true;
+	}
+	return false;
+}
 	
 }
--- src/test/java/com/bridgelabz/addressbooktest/AddressBookTest.java
+++ src/test/java/com/bridgelabz/addressbooktest/AddressBookTest.java
@@ -10,10 +10,18 @@ import com.bridgelabz.employeejdbc.AddressBookService;
 public class AddressBookTest {
 public AddressBookService addressBookService;
 	
-	@Test
-	public void ifData_WhenConnected_ShouldReturnSize() throws SQLException {
-		addressBookService = new AddressBookService();
-		int res = addressBookService.getQuery();
-		Assert.assertEquals(6, res);
-	}
+@Test
+public void ifData_WhenConnected_ShouldReturnSize() throws SQLException {
+	addressBookService = new AddressBookService();
+	int res = addressBookService.getQuery();
+	Assert.assertEquals(6, res);
+}
+
+@Test
+public void ifData_WhenUpdated_ShouldReturnSize() throws SQLException {
+	addressBookService = new AddressBookService();
+	boolean res = addressBookService.getUpdate("Heema","Jetty","heema123@gmail.com");
+	Assert.assertEquals(true, res);
+
+}
 }
